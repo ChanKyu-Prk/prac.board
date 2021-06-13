e87d7c1ae69f23f48525f9541db82e904fd98cf5
DESKTOP-0MO6RQV\tenac <tenacious077@gmail.com>
2021-06-13 15:06:18 +0900

    이클립스로 깃허브 커밋 연습

---
+++ src/user/UserDAO.java
@@ -0,0 +1,60 @@
+package user;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	public UserDAO() { //UserDAO를 통해 데이터베이스에 접근해서 데이터를 다루는 데이터 접근 객체
+		try {
+			String dbURL = "jdbc:mariadb://localhost:3306/bbs";
+			String dbID = "root";
+			String dbPassword = "m1234";
+			Class.forName("org.mariadb.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbID, dbPassword);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public int login(String userID, String userPassword) {
+		String SQL = "select userPassword from user where userID = ?";
+		try {
+			pstmt = conn.prepareStatement(SQL); //sql 인젝션 같은 해킹 기법을 방어하기 위해서 prepareStatement를 이용
+			pstmt.setString(1, userID);
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				if(rs.getString(1).equals(userPassword)) 
+					return 1; //로그인 성공
+				else 
+					return 0; //비밀번호 불일치
+				
+			}
+			return -1; // 아이디가 없음
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -2; //데이터베이스 오류
+	}
+	
+	public int join(User user) {
+		String SQL = "insert into user values (?,?,?,?,?)";
+		try {
+			pstmt = conn.prepareStatement(SQL);
+			pstmt.setString(1, user.getUserID());
+			pstmt.setString(2, user.getUserPassword());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getUserGender());
+			pstmt.setString(5, user.getUserEmail());
+			return pstmt.executeUpdate();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return -1; // 데이터베이스 오류
+	}
+}
